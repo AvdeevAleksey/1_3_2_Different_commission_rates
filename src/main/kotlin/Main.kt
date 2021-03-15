3c96fcb5323cf1e6ed4cebdf9c9d825f169ef0ca
Avdeev Aleksey <avdeev.hexer@yandex.ru>
2021-03-15 20:56:39 +0300

    Optimized the code.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,7 +8,9 @@ fun main() {
     val amountPreviousTransfer:Int = checkMyReadLine()
     print("Введите сумму перевода в копейках: ")
     val amount:Int = readLine()!!.toInt()
-    printMyAmountСommission(cardType, amountPreviousTransfer, amount)
+    println("""Сумма перевода: $amount коп.
+      Комиссия: ${printMyAmountСommission(cardType, amountPreviousTransfer, amount)} коп.
+        """.trimMargin())
 }
 
 fun checkMyReadLine(): Int {
@@ -16,23 +18,21 @@ fun checkMyReadLine(): Int {
     return if (myString.isEmpty()) 0 else myString.toInt()
 }
 
-fun printMyAmountСommission(cardType: Int, amountPreviousTransfer: Int, amount: Int) {
-    val commission:Int = when (cardType) {
-        1 -> {
-            when (amountPreviousTransfer) {
-                in 300 until 75000 -> 0
-                else -> (amount*0.006+2000).toInt()
-            }
-        }
-        2 -> {
-            val minCommission: Int = 3500
-            if (amount*0.0075<=minCommission) minCommission else (amount*0.0075).toInt()
-        }
+fun printMyAmountСommission(cardType: Int, amountPreviousTransfer: Int, amount: Int):Int {
+    return when (cardType) {
+        1 -> giveMyMasterCardMaestro(amountPreviousTransfer, amount)
+        2 -> giveMyVisaMir(amount)
         else -> 0
     }
-    println()
-    println("""Сумма перевода: $amount коп.
-      Комиссия: $commission коп.
-        """.trimMargin())
 }
 
+fun giveMyMasterCardMaestro(amountPreviousTransfer:Int, amount: Int):Int {
+    return when (amountPreviousTransfer) {
+        in 300 until 75000 -> 0
+        else -> (amount * 0.006 + 2000).toInt()
+    }
+}
+
+fun giveMyVisaMir(amount: Int, minCommission: Int = 3500):Int {
+    return if (amount*0.0075<=minCommission) minCommission else (amount*0.0075).toInt()
+}
